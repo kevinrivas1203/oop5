179b7da2768bcab37daeb4d7c7bd04d69ba1e5ff
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-06-30 09:12:47 +0200

    first commid

---
+++ bin/oop5.dart
@@ -0,0 +1,5 @@
+import 'package:oop5/oop5.dart' as oop5;
+
+void main() {
+  print('Hello world');
+}
---
+++ test/oop5_test.dart
@@ -0,0 +1,6 @@
+import 'package:oop5/oop5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  print('Hallo');
+}
